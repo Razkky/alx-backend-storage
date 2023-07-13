d95d7df62d27a76af8bbebaa33586df0130740dc
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-13 18:40:18 +0100

    make commands upper case

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --- Create a table on a database
 
 CREATE TABLE  IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255)
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 )

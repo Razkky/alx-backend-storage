8f337b39b079607980469ebc995bcf149ec0c611
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-13 18:37:07 +0100

    change name of table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --- Create a table on a database
 
-CREATE TABLE  IF NOT EXISTS user(
+CREATE TABLE  IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255)

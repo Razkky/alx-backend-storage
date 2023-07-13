25d87b82e40904d0ae5b212e210809d41341265b
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-13 19:32:45 +0100

    rank country origin of bands

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,9 @@
+-- Rank country origin of bands
+-- Order by number of unique fans
+-- Column names are origin and nb_fans
+
+SELECT origin as origin,
+SUM(fans) AS nb_fans,
+FROM metal_bands,
+GROUP BY origin,
+ORDER BY nb_fans DESC;

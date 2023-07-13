5c5d8ce3d51e2d4251e42aaf470d52655e963a29
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-13 20:09:55 +0100

    list band with glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- List all band with Glam Rock as mainstyle ranked by longivity
 
-SELECT band_name, 
-       (2022 - formed) - (CASE WHEN split = 0 THEN 2022 ELSE split END) AS lifespan
+
+SELECT
+band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;

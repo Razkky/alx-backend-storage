6643b444b462f8b9a9c1989f9d7b17de36553940
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-13 20:46:20 +0100

    valid_email if email is validated

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Creates trigger that reset valid_email Only wen email has been validated
+
+
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+    IF OLD.email == NEW.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;

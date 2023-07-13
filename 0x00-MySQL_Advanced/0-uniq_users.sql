1ed8b9451c697f607692c29f8b67b9bf18808723
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-13 18:35:33 +0100

    check if table exists

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --- Create a table on a database
 
-CREATE TABLE user (
+CREATE TABLE  IF NOT EXISTS user(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255)

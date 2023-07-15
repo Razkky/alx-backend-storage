b096541224255e4e230076897688523af4acb6d6
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-15 15:08:01 +0100

    change year

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 
 
 SELECT
-band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+band_name, ifnull(split, 2022)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;

efcddd5e786ffc9197a13d95e296ee9cfc01d6e8
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-15 17:30:52 +0100

    Creates a function that divides two number

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,4 @@
 -- Creates an index on table names using name and score
-
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+DELIMITER $$
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score)$$
+DELIMITER ;

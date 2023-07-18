e6a90069cde0f73e9302e6ef2445c66f58b5c073
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-18 17:46:40 +0100

    add new line at end of file and fix typo error

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,6 +6,6 @@ def schools_by_topic(mongo_collection, topic) -> list:
     """Return list of all schools having a specific topic"""
 
     schools: list
-    for school in mongo_collection.find({"topic": topic}):
+    for school in mongo_collection.find({"topics": topic}):
         schools.append(school)
     return schools

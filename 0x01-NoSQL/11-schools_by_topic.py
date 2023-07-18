f15b1a70d4f366c4c97f7bf9ac945058f950f4c1
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-18 17:45:36 +0100

    return list of schools having a particular subject

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Return list of schools having a specific  topic"""
+
+
+def schools_by_topic(mongo_collection, topic) -> list:
+    """Return list of all schools having a specific topic"""
+
+    schools: list
+    for school in mongo_collection.find({"topic": topic}):
+        schools.append(school)
+    return schools

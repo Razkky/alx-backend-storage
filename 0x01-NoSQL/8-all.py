8b8be033240553682f3140bdaa45409d61f303dd
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-18 17:25:54 +0100

    return list of all document

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This script uses pymongo to list all document in a collection"""
+from typing import List
+
+
+def list_all(mongo_collection):
+    """Return list of all document in collection"""
+    
+    collection: list = []
+
+    documents = mongo_collection.find()
+    if documents:
+        for doc in documents:
+            collection.append(doc)
+
+    return collection
+
+

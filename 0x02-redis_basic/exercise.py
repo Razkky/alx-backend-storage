f8856edff5cc65f5f6f247a743bdb1b067257a54
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-21 16:42:21 -0700

    implemetn get method along with converstion methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,10 +14,25 @@ class Cache:
         self._redis.flushdb()
 
 
-    def store(self, data: Union[str, int, bytes, float]) -> str :
+    def store(self, data: Union[str, int, bytes, float]) -> str:
         """Takes a data and generate a uuid as the key and the data as its value"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable] = None)\
+            -> Union[str, int, float, bytes]:
+        """Return data to a key converted to a particular format"""
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
 
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        """Convert bytes to str"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """Convert bytes to int"""
+        return int.from_bytes(data, byteorder)

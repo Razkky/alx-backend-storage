0dfd058880894b9850bf75a3f31f07747d4b7ba2
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-22 09:59:39 -0700

    add a call_history wrapper

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,6 +17,22 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """A wrap fucntion that records the history of method
+        Calls in the redis fuction
+    """
+    key = method.__qualname__
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """A wrapper fucntion to map the call history"""
+        input_key = f"{key}:inputs"
+        output_key = f"{key}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return wrapper
+
 class Cache:
     """A class that operates a cachingj system using redit"""
     
@@ -25,7 +41,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes a data and generate a uuid as the key and the data as its value"""
